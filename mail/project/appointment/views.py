d076212a24b2a27dabdf23406245c6e27fa83bac
collotun <collotun@gmail.com>
2022-11-10 14:10:35 +0300

    1

--- mail/project/appointment/views.py
+++ mail/project/appointment/views.py
@@ -1,4 +1,4 @@
 from django.conf import settings
 from django.core.mail import send_mail
 
-send_mail('', 'Доброе утро и хорошего дня (это автоматическая рассылка для автоматизации личных отношений)))', settings.EMAIL_HOST_USER, ['AVPavlenko@nvate.ru', 'collotun@gmail.com'])
+send_mail('', '', settings.EMAIL_HOST_USER, ['collotun@gmail.com'])
